3bb8a67be12c1337aafdeddc85197f15e6eb6035
jorge lisboa <jorgelisboa659@gmail.com>
2023-04-14 02:50:26 -0300

    Register com validação

--- vert-app/screens/Register/index.js
+++ vert-app/screens/Register/index.js
@@ -1,15 +1,14 @@
-import { Button, Input, Text } from '@rneui/themed'
+import { Button, Dialog, Input, Text } from '@rneui/themed'
 import { useState } from 'react'
 import { StyleSheet, ScrollView, KeyboardAvoidingView } from "react-native"
 import { Ionicons } from '@expo/vector-icons'
-import { validateFields } from './validation'
 import { Width } from '../../constants/dimensions'
 import api from '../../Api'
 import VertMaskInput from '../../components/VertMaskInput'
 import { Masks } from 'react-native-mask-input'
 
 export default function Register({navigation}) {
-    
+    const [errorModalVisibility, setErrorModalVisibility] = useState(false)
     const [fullName, setFullName] = useState('')
     const [email, setEmail] = useState('')
     const [password, setPassword] = useState('')
@@ -17,7 +16,42 @@ export default function Register({navigation}) {
     const [whatsapp, setWhatsapp] = useState('')
     const [city, setCity] = useState('')
     const [state, setState] = useState('')
+    const [fieldErrors, setFieldErrors] = useState([])
 
+    function fieldValidator() {
+        let hasErrors = false
+        setFieldErrors([])
+        if(fullName.length < 6) {
+            setFieldErrors(fieldErrors => [...fieldErrors, 'Digite um nome com 6 ou mais caracteres'])
+            hasErrors = true
+        }
+        if (email.length < 6) {
+            setFieldErrors(fieldErrors => [...fieldErrors, 'Digite um email com 6 caracteres ou mais'])
+            hasErrors = true
+        }
+        if(password.length < 6) {
+            setFieldErrors(fieldErrors => [...fieldErrors, 'Digite uma senha com 6 ou mais caracteres'])
+            hasErrors = true
+        }
+        if (whatsapp.length != 15) {
+            setFieldErrors(fieldErrors => [...fieldErrors, 'Digite um telefone válido'])
+            hasErrors = true
+        }
+        if(city.length < 3) {
+            setFieldErrors(fieldErrors => [...fieldErrors, 'Digite uma cidade válida'])
+            hasErrors = true
+        }
+        if (state.length != 3) {
+            setFieldErrors(fieldErrors => [...fieldErrors, 'Digite um email com 6 caracteres ou mais'])
+            hasErrors = true
+        }
+        if(password != confirmPassword) {
+            setFieldErrors(fieldErrors => [...fieldErrors, 'Senhas diferentes, por favor reveja'])
+            hasErrors = true
+        }
+        console.log(fieldErrors)
+        return hasErrors
+    }
     async function tryRegister() {
         await api.post('/signup/', {
             full_name: fullName,
@@ -36,10 +70,21 @@ export default function Register({navigation}) {
         })
     }
     function handleRegister() {
+        if(fieldValidator()) {
+            setErrorModalVisibility(true)
+            return
+        }
         tryRegister()
     }
 
     return(
+        <>
+        { errorModalVisibility ?
+        <Dialog isVisible={errorModalVisibility} onBackdropPress={() => setErrorModalVisibility(false)}>
+            <Dialog.Title titleStyle={styles.modalTitleStyle} title='Atenção!' />
+            {fieldErrors.map((item, i) => <Text h1={true} h1Style={styles.modalItemText} key={i}>- {item}</Text>)}
+        </Dialog>
+        :
         <KeyboardAvoidingView style={styles.loginBox}>
             <ScrollView contentContainerStyle={{ marginTop: 64 }}>
                 <VertMaskInput 
@@ -47,7 +92,7 @@ export default function Register({navigation}) {
                     maxLength={100} 
                     value={fullName} 
                     setValue={setFullName} 
-                    label="full name"
+                    label="nome completo"
                 />
                 <VertMaskInput 
                     leftIcon={<Ionicons size={20} color='#93bf85' name='mail-outline'/>}
@@ -61,14 +106,14 @@ export default function Register({navigation}) {
                     maxLength={30} 
                     value={password} 
                     setValue={setPassword} 
-                    label="password" 
+                    label="senha" 
                 />
                 <VertMaskInput 
                     leftIcon={<Ionicons size={20} color='#93bf85' name='key-outline'/>}
                     maxLength={30} 
                     value={confirmPassword} 
                     setValue={setConfirmPassword} 
-                    label="Confirmar senha" 
+                    label="confirmar senha" 
                 />
                 <VertMaskInput 
                     leftIcon={<Ionicons size={20} color='#93bf85' name='logo-whatsapp'/>}
@@ -84,19 +129,21 @@ export default function Register({navigation}) {
                     maxLength={30} 
                     value={city} 
                     setValue={setCity} 
-                    label="city" 
+                    label="cidade" 
                 />
                 <VertMaskInput 
                     leftIcon={<Ionicons size={20} color='#93bf85' name='location-outline'/>}
                     maxLength={2} 
                     value={state} 
                     setValue={setState} 
-                    label="state" 
+                    label="estado" 
                 />
             </ScrollView>
 
             <Button onPress={handleRegister}>Cadastrar</Button>
         </KeyboardAvoidingView>
+        }
+        </>
     )
 }
 
@@ -104,6 +151,16 @@ const styles = StyleSheet.create({
     forgotPassword: {
       marginVertical: 16,
       fontSize: 16,
+    },
+    modalItemText: {
+        fontSize: 16,
+        fontWeight: 'bold',
+        marginBottom: 16,
+    },
+    modalTitleStyle: {
+        fontWeight: 'bold',
+        fontSize: 40,
+        marginBottom: 32,
     },
     loginBox: {
         flex: 1,
